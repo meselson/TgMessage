6638c5be3e2aadf3ef10fc2990c22a975a5bc9aa
meselson <meselson@126.com>
2021-02-26 12:23:19 +0800

    Update setWebHook.php

--- api/setWebHook.php
+++ api/setWebHook.php
@@ -4,7 +4,7 @@ header('content-type: application/json');
 $key = $_REQUEST['key'] ?? '';
 
 if ($key !== $_ENV['key']) {
-    echo json_encode(['code' => 422, 'message' => '设置秘钥错误']);
+    echo json_encode(['code' => 422, 'message' => 'key error']);
 } else {
     //开始设置回调url
     $url = $_REQUEST['url'] ?? $_ENV['url'];
@@ -13,7 +13,7 @@ if ($key !== $_ENV['key']) {
     $bot = new Bot();
     $ret = $bot->setWebHook($data);
     if ($ret['ok']) {
-        echo json_encode(['code' => 200, 'message' => '设置成功']);
+        echo json_encode(['code' => 200, 'message' => 'config success']);
     } else {
         echo json_encode(['code' => 422, 'message' => $ret['description']]);
     }
