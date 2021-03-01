36942e99b133ae1e5e979dbed06e858f48c73cf3
meselson <meselson@126.com>
2021-03-01 17:07:41 +0800

    Update webhook.php

--- api/webhook.php
+++ api/webhook.php
@@ -8,7 +8,7 @@ $chat_id = $data['message']['chat']['id'];
 $message = $data['message']['text'] ?? '';
 if ($message === '/token') {
     $bot->sendMessage(['text' => $bot->encryption($chat_id), 'chat_id' => $chat_id]);
-} elseif ($message === '/help') {
+} elseif ($message === '/help' || '/start') {
     $bot->sendMessage(['text' => "1. 发送 /token 获取个人token\n2. 请求地址 https://tgpush.vercel.app/api\n3. 调用方式 HTTP get post\n4. get构建方式 https://tgpush.vercel.app/api?token=".$bot->encryption($chat_id)."&message=推送信息\n5. 消息如需换行，请在需换行内容之间添加 %0A", 'chat_id' => $chat_id]);
 } else {
     $bot->sendMessage(['text' => 'TG君不理解您的信息！', 'chat_id' => $chat_id]);
